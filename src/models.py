0b6abedabbdff810d82f601355a98d157ecc7bb9
Hector Ferrer <72463386+Hec17v@users.noreply.github.com>
2026-05-28 13:17:48 +0000

    segunda subida

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from eralchemy2 import render_er
 
 db = SQLAlchemy()
 
@@ -20,7 +21,31 @@ class User(db.Model):
         }
     
 
-class Charater(db.Model):
+class Character(db.Model):
     __tablename__ ='character'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
+    favorites: Mapped[list["Favorite"]] = relationship(back_populates ="planet")
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    fovorites: Mapped[list["Favorite"]] = relationship(back_populates="planet")
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    charcter_id: Mapped[int | None] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    planet_id: Mapped[int | None] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    character: Mapped["Character"] = relationship(back_populates="favorites")
+    planet: Mapped["Planet"] = relationship(back_populates="favorites")
+
+
+try:
+    render_er(db.Model, 'diagram.png')
+    print("Diagrama generado exitosamente como diagram.png")
+except Exception as e:
+    print(f"Error al generar el diagrama: {e}")
